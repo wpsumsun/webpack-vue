265ee5c2670aa09d502fcc099d64ca6313ea0e80
summer <wpsumsun@126.com>
2017-07-16 22:11:23 +0800

    添加注册登录登出功能

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 import bar from './bar';
 import Vue from 'vue'
+import AV from 'leancloud-storage'
+
+var APP_ID = 'DMA6DHsdgOvdC4JtJ25FlyDw-gzGzoHsz';
+var APP_KEY = 'LQXyVN6bwHDEkxwvNhGi8hpl';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
 
 var app=new Vue({
     el:'#app',
     data:{
-        newTodo:"",
-        todoList:[]
+        actionType:'signUp',
+        formData:{
+            username:'',
+            password:''
+        },
+        newTodo:'',
+        todoList:[],
+        currentUser:null,
     },
     created:function(){
         window.onbeforeunload=()=>{
@@ -30,6 +44,38 @@ var app=new Vue({
         removeTodo:function(todo){
             let index=this.todoList.indexOf(todo)
             this.todoList.splice(index,1)
+        },
+        signUp:function(){
+            var user = new AV.User();
+            // 设置用户名
+            user.setUsername(this.formData.username);
+            // 设置密码
+            user.setPassword(this.formData.password);
+            // 设置邮箱
+            // user.setEmail('tom@leancloud.cn');
+            user.signUp().then( (loginedUser)=> {
+                this.currentUser=this.getCurrentUser()
+            }, function (error) {
+                alert('注册失败')
+            });
+
+        },
+        login:function(){
+             AV.User.logIn(this.formData.username, this.formData.password).then( (loginedUser)=> {
+                this.currentUser=this.getCurrentUser()
+            }, function (error) {
+            });
+        },
+        getCurrentUser:function(){
+            let {id,createAt,attributes:{username}}=AV.User.current()
+
+            return {id,username,createAt}
+        },
+        logout:function(){
+            AV.User.logOut();
+            // 现在的 currentUser 是 null 了
+            this.currentUser = null;
+            window.location.reload()
         }
     }
 })
